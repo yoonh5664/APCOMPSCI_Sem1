5c8af51abdbaefb1430db4829c7ed1315b9de54b
yoonh5664 <harimyoon0916@gmail.com>
2016-09-02 11:27:36 -0700

    Added changes

---
+++ APLesson_01/ASCIIART_01.java
@@ -0,0 +1,16 @@
+public class ASCIIART_01
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println("           	                __   ");
+		System.out.println("_____     ________/  |_ ");
+		System.out.println("\\\\__     \\\\\\\\_   __   \\\\       __\\\\");
+		System.out.println(" / __     \\\\ |     |  \\\\/|      |  ");
+		System.out.println("(____   /   __ |      |__  |  ");
+		System.out.println("        \\\\/             ");
+		
+		
+}
+}
---
+++ APLesson_01/ASCIIART_02.java
@@ -0,0 +1,14 @@
+public class ASCIIART_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                        __           ");
+		System.out.println("   ____ _____  |    |  __ ____  ");
+		System.out.println("_/ ___\\\\__    \\  |    | /   // __ \\ ");
+		System.out.println("\\   \\___  / __  \\ |        <\\  ___/ ");
+		System.out.println(" \\___   >____  /__|_   \\\\___ >");
+		System.out.println("         \\/         \\/          \\/        \\/ ");
+		System.out.println("");
+		System.out.println("");
+	}
+}
