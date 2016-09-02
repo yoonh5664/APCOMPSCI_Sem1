df02dd409d8028e142d2e5216592114bbb009413
yoonh5664 <harimyoon0916@gmail.com>
2016-09-02 11:01:48 -0700

    Added Files

---
+++ APLesson_01/firstProg.java
@@ -0,0 +1,22 @@
+public class firstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Hello Again");
+		System.out.println("           	                __   ");
+		System.out.println("_____     ________/  |_ ");
+		System.out.println("\\\\__     \\\\\\\\_   __   \\\\       __\\\\");
+		System.out.println(" / __     \\\\ |     |  \\\\/|      |  ");
+		System.out.println("(____   /   __ |      |__  |  ");
+		System.out.println("        \\\\/             ");
+		System.out.println("                        __           ");
+		System.out.println("   ____ _____  |    |  __ ____  ");
+		System.out.println("_/ ___\\\\__    \\  |    | /   // __ \\ ");
+		System.out.println("\\   \\___  / __  \\ |        <\\  ___/ ");
+		System.out.println(" \\___   >____  /__|_   \\\\___ >");
+		System.out.println("         \\/         \\/          \\/        \\/ ");
+		System.out.println("");
+		System.out.println("");
+	}
+}
