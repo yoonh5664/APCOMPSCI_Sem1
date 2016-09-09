870fb4fb0f395e55fd49707170428989b824fcf8
yoonh5664 <harimyoon0916@gmail.com>
2016-09-09 10:54:05 -0700

    added changes

---
+++ APLesson_01/APLesson_02/APLesson_02.java
@@ -0,0 +1,8 @@
+public class APLesson_02
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);
+	}
+}
---
+++ APLesson_01/APLesson_02/Lab_02.java
@@ -0,0 +1,47 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 2;
+		int b = 4;
+		int recArea = a * b;
+		
+		System.out.println("2 multiplied by 4 is " + recArea);
+		
+		int c = 5;
+		int d = 6;
+		int recArea2 = c * d;
+		
+		System.out.println("5 multiplied by 6 is " + recArea2);
+		
+		String third = "Name";
+		
+		String fourth = "Address";
+	
+		String fifth = "City";
+		
+		String sixth = "Zip";
+		
+		System.out.println(third);
+		System.out.println(fourth);
+		System.out.println(fifth);
+		System.out.println(sixth);
+		
+		int l = 3;
+		int w = 4;
+		int h = 2;
+		int recSurfacearea = 2 * w * l + 2 * l * h + 2 * h * w;
+		
+		System.out.println("The surface area of your rectangle is" + recSurfacearea);
+		
+		String first = "Harim";
+		String second = " Yoon";
+		
+		System.out.println(first + second);
+		
+		
+		
+		
+	}
+}	
+	
