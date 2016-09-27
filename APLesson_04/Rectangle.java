3ba215ddcaa899bf472cd1fe0cddd353a942a887
yoonh5664 <harimyoon0916@gmail.com>
2016-09-27 12:14:57 -0700

    addedchanges

---
+++ APLesson_04/Rectangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Rectangle
+{
+	static double l;
+	static double w;
+	static double perimeter;
+	
+	public static void main(String[]args)
+	{
+		//user inpout for l and w
+		//call cakPerim()
+		//call print()
+	}
+	
+	public static void calcPerim()
+	{
+		//all tgge math for the perimeter goes here
+		perimeter = //do the math 
+	}
+	public static void print()
+	{
+		//simple print statement
+		
+		
+	}
+}
