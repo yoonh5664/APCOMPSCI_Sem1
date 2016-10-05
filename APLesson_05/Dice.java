b454e5f1678d7d6c36aaa2d87ffa950263252ff2
yoonh5664 <harimyoon0916@gmail.com>
2016-10-05 12:13:44 -0700

    added changes

--- APLesson_05/Dice.java
+++ APLesson_05/Dice.java
@@ -4,10 +4,19 @@ public class Dice
 	public static void main(String[]args)
 	{
 		Random rand = new Random();
-		int num = rand.nextInt(6)+1;
-		System.out.println("You rolled a " + num);
+		int num1 = rand.nextInt(6)+1;
+		System.out.println("You rolled a " + num1);
+		
 		int num2 = rand.nextInt(6)+1;
 		System.out.println("Computer rolled a " + num2);
-		
+		System.out.println("The winner is );
+		if (num1 > num2)
+		{
+			System.out.println(" The winner is you );
+		}
+		if (num2 > num1)
+		{
+			System.out.println(" The winner is the computer );
+		}
 	}
 }
