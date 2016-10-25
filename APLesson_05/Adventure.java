370d477002185e4b59f5285bae13721fdf12483c
yoonh5664 <harimyoon0916@gmail.com>
2016-10-25 12:11:32 -0700

    added changes

--- APLesson_05/Adventure.java
+++ APLesson_05/Adventure.java
@@ -2,37 +2,25 @@ import java.util.Scanner;
 public class Adventure
 {
 	static Scanner kb;
-	public static void main(String[]args)
+	public static void main (String[]args);
 	{
 		kb = new Scanner(System.in);
-		
+		System.out.println("Welcome to Adventure World where you get to choose your own adventure. Would you like to..." +
+							"\n 1. Take a walk" +
+							"\n 2. Stay at home");
+		int choice = kb.nextInt();
+		if(choice = 1)
+		{
+			System.out.println("Would you like to..." +
+								"\n a. Walk to a friend's house" +
+								"\n b. Take a stroll around the neighborhood" +
+								"\n c. Walk to a market");
+			String answer = kb.next();
+			if (answer.equals == a)
+			{
+				System.out.println("You run into a zombie. The zombie eats you and you die.");
+			}	
+		}	
 	}
-}
-if(choice == choice1)
-	//prompt for choice 
-	if (choice == a)
-		//prompt for choice
-		if (choice == b)
-			//end of program
-		else //end of program 
-	//prompt for choice
-	else 
-		//prompt for choice 
-		if (choice == c )
-			//end of program 
-		else // end of program
-		
-else //<part of choice 1>
-	//prompt for choice 
-	if (choice == a)
-		//prompt for choice 
-		if (choice == b)
-				//end of program 
-		else //end of program
-	//prompt for choice 
-	else 
-		//prompt for choice 
-		if (choice == c)
-			//end of program 
-		else  // end of program 
-	
+	if
+}	
--- APLesson_05/practice.java
+++ APLesson_05/practice.java
@@ -55,3 +55,61 @@ public class practice
 		System.out.printf("\n* %15s ........ %10.2f ", item, price);
 	}
 }
+
+import java.util.Scanner;
+public class Adventure
+{
+	static Scanner kb;
+	public static void main(String[]args)
+	{
+		kb = new Scanner(System.in);
+		System.out.println("Welcome to Adventure World where you get to choose your own adventure. Would you like to..." +
+							"\n 1. Take a walk" +
+							"\n 2. Stay at home");
+		int choice = kb.nextInt();			
+		if(choice == 1)
+			System.out.println("Would you like to..." +
+								"\n a. Walk to a friend's house" +
+								"\n b. Take a stroll around the neighborhood" +
+								"\n c. Walk to a market");
+								
+			String answer = kb.next();
+		{
+			if (choice.equals("a"))
+			{
+				System.out.println("You run into a zombie. The zombie eats you and you die.");
+				if (choice.equals("b"))
+					System.out.println("You pass by a cute dog. Would you like to pet it?");
+				else 
+					System.out.println("");
+			}
+			else 
+			{	
+				System.out.println("You walk to a market.");
+				if (choice.equals("c"))
+					System.out.println("You buy food at the market and you are forever happy!");
+				else 
+					System.out.println("");
+			}
+		}	
+		else 
+			System.out.println("Would you like to..." +
+								"\n a. Take a nap" +
+								"\n b. Play video games" +
+								"\n c. Go on the computer");
+			if(choice.equals("a"))
+			{
+					System.out.println("You feel very happy and energized after your nap");
+				if (choice.equals("b"))
+					System.out.println("You feel very tired from looking at the screen for many hours");
+				else 
+					System.out.println("");
+			}
+			else 
+				System.out.println("You go on the computer");
+				if (choice.equals("c"))
+					System.out.println("You feel very happy from talking to your friends online");
+				else 
+					System.out.println("");
+		}				
+}
