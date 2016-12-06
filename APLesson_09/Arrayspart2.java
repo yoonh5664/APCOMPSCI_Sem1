e00c77a59dd99567af5d139f77d07c871f47ad8b
yoonh5664 <harimyoon0916@gmail.com>
2016-12-06 12:00:54 -0800

    added changes

--- APLesson_09/Arrayspart2.java
+++ APLesson_09/Arrayspart2.java
@@ -3,22 +3,22 @@ public class Arrayspart2
 {
 	public static void main(String[]args)
 	{
-		int[] number = new int[10];
+		int[] numbers = new int[10];
 		
-		for(int i = 0; i < names.length; i++)
+		for(int i = 0; i < numbers.length; i++)
 		{
-			names[i]= (int)(Math.random()*100) + 1;
+			numbers[i]= (int)(Math.random() * 100) + 1;
 		}
 		
 		for(int num : numbers)
 			System.out.println(num);
 		
 		System.out.println();
-		System.out.println("The sum of the numbers aboves is..." +sumArray(numbers));
+		System.out.println("The sum of the numbers aboves is..." + sumArray(numbers));
 	}
 	public static int sumArray(int[] n)
 	{
-		int sum = 0
+		int sum = 0;
 		for(int num : n)
 		{
 			sum +=num;
