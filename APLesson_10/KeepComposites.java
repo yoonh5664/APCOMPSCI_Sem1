9d19d991695854429276b02dc93095b555074951
yoonh5664 <harimyoon0916@gmail.com>
2016-12-16 12:13:19 -0800

    added changes

---
+++ APLesson_10/KeepComposites.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class KeepComposites
+{
+	public static void main(String[]args)
+	{
+		Integer [] numbers = {1,2,3,4,5};
+		ArrayList<Integer> nums = new ArrayList<> (Arrays.asList(numbers));
+		
+		System.out.println(nums);
+	}
+}
