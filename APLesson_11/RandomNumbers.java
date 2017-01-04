8c5899dd68e1e23ba70d53aa86eb067dd535f7d3
yoonh5664 <harimyoon0916@gmail.com>
2017-01-04 12:12:11 -0800

    added changes

---
+++ APLesson_11/RandomNumbers.java
@@ -0,0 +1,13 @@
+public class RandomNumbers
+{
+	public static void main(String[]args)
+	{
+		String letters = "abcdefghijklmnopqrstuvwxyz";
+		String [][] nums = new String[4][4];
+		
+		num[0][0] = 
+		num[1][1] =
+		num[2][2] =
+		num[3][3] = 
+	}
+}
