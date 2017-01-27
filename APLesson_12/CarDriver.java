5fa36369ab4a7171dcca6c2ac1b90908302de7ec
yoonh5664 <harimyoon0916@gmail.com>
2017-01-27 10:22:19 -0800

    added changes

--- APLesson_12/CarDriver.java
+++ APLesson_12/CarDriver.java
@@ -18,7 +18,7 @@ public class CarDriver
 		System.out.println("Your vehicle is ready... ");
 		System.out.println("Paint: " + object.getPaint());
 		System.out.println("Interior: " + object.getInterior());
-		System.out.println("Engine: " + object.getEnging());
+		System.out.println("Engine: " + object.getEngine());
 		System.out.println("Tires: " + object.getTires());
 	}
 }
